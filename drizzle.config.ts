f5871a58d46769d8e916fbc591ed85770ebc57f3
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-02 00:02:00 -0500

    feat: update database configuration to use SingleStore and implement connection pooling

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { type Config } from "drizzle-kit"
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { env } from "~/env"
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["boxcloud_*"],
 } satisfies Config
