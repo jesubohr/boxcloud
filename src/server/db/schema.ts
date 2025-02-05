49d5167f400738c196a56802f5ab1d4419faebdc
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-04 23:26:16 -0500

    feat: define folders and files tables in schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,30 +1,30 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
+import { int, bigint, text, date, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core"
 
-import { sql } from "drizzle-orm"
-import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core"
-
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = sqliteTableCreator((name) => `boxcloud_${name}`)
-
-export const posts = createTable(
-  "post",
-  {
-    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-    name: text("name", { length: 256 }),
-    createdAt: int("created_at", { mode: "timestamp" })
-      .default(sql`(unixepoch())`)
-      .notNull(),
-    updatedAt: int("updated_at", { mode: "timestamp" }).$onUpdate(
-      () => new Date(),
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  }),
+export const createTable = singlestoreTableCreator(
+  (name) => `boxcloud_${name}`,
 )
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }),
+  created_at: date("created_at").notNull(),
+}, (table) => {
+  return [
+    index("parent_index").on(table.parent),
+  ]
+})
+
+export const files = createTable("files_table", {
+  id: bigint("id", { mode: "number", unsigned: true }).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  type: text("type").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  created_at: date("created_at").notNull(),
+}, (table) => {
+  return [
+    index("parent_index").on(table.parent),
+  ]
+})
