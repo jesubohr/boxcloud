225112a3c57b688727c0c106eef466379f266acc
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-05 00:17:34 -0500

    feat: add utility functions for date formatting and size conversion in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date)
+}
+
+export function formatSize(bytes: number) {
+  const sizes = ["B", "KB", "MB", "GB", "TB"]
+  if (bytes === 0) return "0 B"
+  const i = Math.floor(Math.log2(bytes) / 10)
+  const rawsSize = bytes / Math.pow(1024, i)
+  const fixedSize = Number.isInteger(rawsSize) ? rawsSize : rawsSize.toFixed(2)
+  return `${fixedSize} ${sizes[i]}`
+}
