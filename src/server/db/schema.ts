1db762ea5b47d49fcf49b67ff9c751488b6a21df
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-05 00:18:08 -0500

    feat: add server-only import to schema for enhanced server-side functionality

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,3 +1,5 @@
+import "server-only";
+
 import { int, bigint, text, date, index, singlestoreTableCreator } from "drizzle-orm/singlestore-core"
 
 export const createTable = singlestoreTableCreator(
