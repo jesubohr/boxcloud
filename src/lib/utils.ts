67f1adcff93b34a961a45547c119511eb23c0cf7
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-08 02:43:45 -0500

    fix: update date formatting to use default locale

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat("en-US", {
+  return new Intl.DateTimeFormat(undefined, {
     year: "numeric",
     month: "long",
     day: "numeric",
