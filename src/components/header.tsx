5aeec262b7109957a1d356687f1d37113df68391
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-08 01:45:59 -0500

    feat: integrate Clerk for authentication and add middleware

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,6 @@
 import { Upload } from "lucide-react"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+
 import { Button } from "~/components/ui/button"
 
 export function Header() {
@@ -6,9 +8,17 @@ export function Header() {
     <header className="bg-gray-800 py-4">
       <div className="container mx-auto flex items-center justify-between px-4">
         <h1 className="text-2xl font-bold">Google Drive Clone</h1>
-        <Button variant="outline">
-          <Upload className="mr-2 h-4 w-4" /> Upload
-        </Button>
+        <div className="flex items-center gap-8">
+          <Button>
+            <Upload className="mr-2 h-4 w-4" /> Upload
+          </Button>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
       </div>
     </header>
   )
