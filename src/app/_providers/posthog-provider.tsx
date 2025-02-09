898a330e7a437336fe7d65b3c636be3aa4e21991
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-08 20:29:22 -0500

    feat: add PostHog API rewrites and update provider configuration

--- src/app/_providers/posthog-provider.tsx
+++ src/app/_providers/posthog-provider.tsx
@@ -7,7 +7,8 @@ import { env } from "~/env"
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   })
 }
