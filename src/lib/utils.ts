b6796ed46e942040a6129928b869bc74d8cb550d
Jesus Borrero <jesufer2018@hotmail.com>
2025-02-08 20:58:22 -0500

    feat: set default locale to "en-US" in formatDate function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(date: Date, locale?: string) {
+export function formatDate(date: Date, locale = "en-US") {
   return new Intl.DateTimeFormat(locale, {
     year: "numeric",
     month: "long",
